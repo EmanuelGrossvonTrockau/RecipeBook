17dcbfd864ca938519453ee102ade44c1a3a9c0f
Emanuel Gross von Trockau <egrossvontrockau@lcs.on.ca>
2023-06-07 19:52:23 -0400

    Added a structure

---
+++ RecipeBook/Model/Recipe.swift
@@ -0,0 +1,18 @@
+//
+//  Recipe.swift
+//  RecipeBook
+//
+//  Created by Emanuel Gross von Trockau on 2023-06-07.
+//
+import Blackbird
+import Foundation
+
+struct Recipe: BlackbirdModel {
+     
+    @BlackbirdColumn var id: Int
+    @BlackbirdColumn var title: String
+    @BlackbirdColumn var steps: String
+    @BlackbirdColumn var ingredients: String
+    
+    
+}
