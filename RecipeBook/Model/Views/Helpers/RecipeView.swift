4ffca33681c071f5723a1a68fc68b307885e0cfb
Emanuel Gross von Trockau <egrossvontrockau@lcs.on.ca>
2023-06-07 19:28:41 -0400

    Started on my interface

---
+++ RecipeBook/Model/Views/Helpers/RecipeView.swift
@@ -0,0 +1,40 @@
+//
+//  RecipeView.swift
+//  RecipeBook
+//
+//  Created by Emanuel Gross von Trockau on 2023-06-06.
+//
+
+import SwiftUI
+
+struct RecipeView: View {
+    let title: String
+    let steps: String
+    let ingredients: String
+    
+    var body: some View {
+        
+        VStack {
+            
+            Text(title)
+                .font(.title3)
+                .bold()
+            Text("_________________________________________")
+            Text(steps)
+            Text("_________________________________________")
+            Text(ingredients)
+            Text("___________________________________")
+                
+            Spacer()
+                
+            
+        }
+        .padding(15)
+    }
+}
+
+struct RecipeView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeView(title: "Tomato Pasta", steps: "1. Boil the water and add salt to it 2. cook the pasta 3. Add tomato soup ", ingredients: "Pasta,salt, water, Tomato soup")
+    }
+}
