4893fb5be80587b35f4e911dd88b00a61344e462
Emanuel Gross von Trockau <egrossvontrockau@lcs.on.ca>
2023-06-07 19:46:50 -0400

    created the static layout for the app

--- RecipeBook/RecipeListView.swift
+++ RecipeBook/RecipeListView.swift
@@ -10,15 +10,19 @@ import SwiftUI
 struct RecipeListView: View {
     var body: some View {
         NavigationView{
-            VStack{
-                
+            List{
+                RecipeView(title: "Choclate Cake", steps: "1, Get a Bowl and add all ingredients. 2. Mix everything together.                 3. Bake it for 30 min", ingredients: "Milk, Eggs, flour, choclate powder ")
+                RecipeView(title: "Spagetthi Cabonara", steps: "1. Boil Hot water, add salt. 2. Prepare your souce, mix the eggs with some seasening and speck. 3. Cook the nudels 4. add the souce to the nudels after their done. ", ingredients: "Pasta, Eggs,Speck, Salt")
             }
         }
+        .navigationTitle("Recipe Book")
     }
 }
 
 struct RecipeListView_Previews: PreviewProvider {
     static var previews: some View {
-        RecipeListView()
+        NavigationView {
+            RecipeListView()
+        }
     }
 }
