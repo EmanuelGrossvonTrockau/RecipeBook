1e80cd19ca13f631eaa0208cd172e667d06fc096
Emanuel Gross von Trockau <egrossvontrockau@lcs.on.ca>
2023-06-06 20:04:45 -0400

    Initial Commit

---
+++ RecipeBook/RecipeBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBookApp.swift
+//  RecipeBook
+//
+//  Created by Emanuel Gross von Trockau on 2023-06-06.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
